0def8c6a8f0d99f5af9e5b5cbadcefd7a9fdbab1
Anna Ho <llanna.holl@gmail.com>
2016-05-14 12:23:16 +0800

    * add validation for start date and end date

--- app/models/group_event.rb
+++ app/models/group_event.rb
@@ -6,7 +6,16 @@ class GroupEvent < ActiveRecord::Base
   validates_presence_of :location, if: lambda{ |event|event.published? }
   validates_presence_of :start_date, if: lambda{ |event| event.published? }
   validates_presence_of :end_date, if: lambda { |event| event.published? }
+  validate :end_date_after_start_date
 
+  def end_date_after_start_date
+    return if end_date.blank? || start_date.blank?
+
+    if end_date < start_date
+      errors.add(:end_date, "cannot be before the start date") 
+    end 
+  end
+  
   def draft?
     status == "draft"
   end
--- spec/model/group_event_spec.rb
+++ spec/model/group_event_spec.rb
@@ -3,6 +3,13 @@ require 'rails_helper'
 describe GroupEvent, 'Validations', :type => :model do
   
   it { is_expected.to validate_presence_of(:status)}
+  
+  it 'does not allow start date to be after end date' do
+    start_date = DateTime.now
+    end_date = start_date - 1.days
+    group_event = GroupEvent.new(name: "an event", status: "draft", start_date: start_date, end_date: end_date)
+    expect(group_event.valid?).to be false  
+  end
 
   context "published?" do
     before { allow(subject).to receive(:published?).and_return(true) }
