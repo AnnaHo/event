66d8ab07ad1513377010551ff8cd44d087a2f954
Anna Ho <llanna.holl@gmail.com>
2016-05-14 10:54:25 +0800

    * add create action to create group event   * add test case for creating a draft group event

--- app/controllers/api/v1/group_events_controller.rb
+++ app/controllers/api/v1/group_events_controller.rb
@@ -4,4 +4,19 @@ class Api::V1::GroupEventsController < ApiController
     render json: GroupEvent.all
   end
 
+  def create
+    group_event = GroupEvent.new(group_event_params)
+
+    if group_event.save
+      render json: group_event
+    else
+      render json: {message: "failed to create event", errors: group_event.errors.full_messages}, status: 422
+    end
+  end
+
+  private
+
+  def group_event_params
+    params.require(:group_event).permit(:name, :description, :location, :start_date, :end_date, :duration, :status)
+  end
 end
--- spec/request/api/v1/group_events_spec.rb
+++ spec/request/api/v1/group_events_spec.rb
@@ -8,7 +8,18 @@ describe 'GET /v1/group_events', type: :request do
 
   it 'get all group_events' do
     get '/v1/group_events'
-    result = JSON.parse(response.body)
-    expect(result.size).to be 1
+    expect(response_json.size).to be 1
   end
+end
+
+describe 'POST /v1/group_events', type: :request do
+  it 'create an event with draft status' do
+    post '/v1/group_events', {group_event: {name: "a new event", status: "draft"}}
+    group_event = GroupEvent.last
+    expect(response_json).to eq(JSON.parse(group_event.to_json))
+  end
+end
+
+def response_json
+  JSON.parse(response.body)
 end
